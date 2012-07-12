1e23e7c25f82e88afddd839b97681c0dbc9bff68
DonIvanCorleone <donivancorleone@gmx.de>
2012-07-12 15:04:17 +0300

    Update master

--- lib/google_places_autocomplete.rb
+++ lib/google_places_autocomplete.rb
@@ -1,7 +1,7 @@
 require 'rubygems'
 require 'httparty'
 require 'hashie'
-require File.join(File.dirname(__FILE__), 'google_places_autocomplete', 'client')
+require File.join(File.dirname(__FILE__), 'google_places_autocomplete_fork', 'client')
 
 module GooglePlacesAutocomplete
   def self.configure
